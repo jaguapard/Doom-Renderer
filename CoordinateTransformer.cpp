bd9bc34fdfe439c8dade94a35f61d3c917da1c16
jaguapard <77740043+jaguapard@users.noreply.github.com>
2024-04-10 12:03:12 +0500

    Coordinate transformer individual transformation methods

--- CoordinateTransformer.cpp
+++ CoordinateTransformer.cpp
@@ -5,7 +5,7 @@ CoordinateTransformer::CoordinateTransformer(int w, int h)
 	this->w = w;
 	this->h = h;
 	double widthToHeightAspectRatio = double(w) / h;
-	shift = { widthToHeightAspectRatio / 2, 0.5, 0 };
+	this->_shift = { widthToHeightAspectRatio / 2, 0.5, 0 };
 }
 
 void CoordinateTransformer::prepare(const Vec3& camPos, const Matrix3& rotation)
@@ -20,7 +20,22 @@ Vec3 CoordinateTransformer::toScreenCoords(const Vec3& v) const
 	Vec3 rot = rotation * camOffset;
 	Vec3 perspective = rot / rot.z; //screen space coords of vector
 
-	Vec3 shifted = perspective + this->shift; //convert so (0,0) in `perspective` corresponds to center of the screen
+	Vec3 shifted = perspective + this->_shift; //convert so (0,0) in `perspective` corresponds to center of the screen
 	Vec3 final = shifted * h;
 	return final;
+}
+
+Vec3 CoordinateTransformer::doCamOffset(const Vec3& v) const
+{
+	return v - camPos;
+}
+
+Vec3 CoordinateTransformer::rotate(const Vec3& v) const
+{
+	return rotation * v;
+}
+
+Vec3 CoordinateTransformer::shift(const Vec3& v) const
+{
+	return v + this->_shift;
 }
--- CoordinateTransformer.h
+++ CoordinateTransformer.h
@@ -7,10 +7,14 @@ public:
 	CoordinateTransformer(int w, int h);
 	void prepare(const Vec3& camPos, const Matrix3& rotation);
 	Vec3 toScreenCoords(const Vec3& v) const;
+
+	Vec3 doCamOffset(const Vec3& v) const;
+	Vec3 rotate(const Vec3& v) const;
+	Vec3 shift(const Vec3& v) const;
 private:
 	int w, h;
 	Vec3 camPos;
 	Matrix3 rotation;
-	Vec3 shift;
+	Vec3 _shift;
 	double widthToHeightAspectRatio;
 };

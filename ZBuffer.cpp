0efb8ff53c01948e879ef3226fc75b18596afd7c
jaguapard <77740043+jaguapard@users.noreply.github.com>
2024-04-13 16:50:29 +0500

    Removed never triggering conditions from Z buffer's tests

--- ZBuffer.cpp
+++ ZBuffer.cpp
@@ -8,16 +8,6 @@ ZBuffer::ZBuffer(int w, int h) : PixelBuffer<double>(w, h)
 bool ZBuffer::testAndSet(int x, int y, double depth)
 {
 	StatCount(statsman.zBuffer.depthTests++);
-	if (depth > 0)
-	{
-		StatCount(statsman.zBuffer.negativeDepthDiscards++);
-		return false; //disallow stuff from behind the camera to show up, kinda?
-	}
-	if (isOutOfBounds(x, y))
-	{
-		StatCount(statsman.zBuffer.outOfBoundsAccesses++);
-		return false;
-	}
 	bool cmp = depth < this->getPixelUnsafe(x, y);
 	if (cmp)
 	{

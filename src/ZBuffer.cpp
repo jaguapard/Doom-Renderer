0011f2f91bb3d299458f29dd0869dda4f0a32bee
jaguapard <77740043+jaguapard@users.noreply.github.com>
2024-04-14 12:22:35 +0500

    Added ZBuffer::test

--- src/ZBuffer.cpp
+++ src/ZBuffer.cpp
@@ -5,10 +5,16 @@ ZBuffer::ZBuffer(int w, int h) : PixelBuffer<double>(w, h)
 {
 }
 
-bool ZBuffer::testAndSet(int x, int y, double depth)
+bool ZBuffer::test(int x, int y, double depth)
 {
 	StatCount(statsman.zBuffer.depthTests++);
 	bool cmp = depth < this->getPixelUnsafe(x, y);
+	return cmp;
+}
+
+bool ZBuffer::testAndSet(int x, int y, double depth)
+{
+	bool cmp = test(x, y, depth);
 	if (cmp)
 	{
 		StatCount(statsman.zBuffer.writes++);
--- src/ZBuffer.h
+++ src/ZBuffer.h
@@ -7,5 +7,6 @@ class ZBuffer : public PixelBuffer<double>
 {
 public:
 	ZBuffer(int w, int h);
+	bool test(int x, int y, double depth);
 	bool testAndSet(int x, int y, double depth);
 };

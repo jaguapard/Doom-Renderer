d775bb0df7c5c4e23533cccf3b3b8069a0f81a29
jaguapard <77740043+jaguapard@users.noreply.github.com>
2024-04-15 23:54:20 +0500

    Added 3 value constructor to Color

--- src/Color.cpp
+++ src/Color.cpp
@@ -19,6 +19,14 @@ Color::Color(uint32_t u)
 	*this = *reinterpret_cast<const Color*>(&u);
 }
 
+Color::Color(uint8_t r, uint8_t g, uint8_t b)
+{
+	this->r = r;
+	this->g = g;
+	this->b = b;
+	this->a = SDL_ALPHA_OPAQUE;
+}
+
 Color::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 {
 	this->r = r;
--- src/Color.h
+++ src/Color.h
@@ -5,6 +5,7 @@ struct Color : SDL_Color
 {
 	Color() = default;	
 	Color(uint32_t u); //pixel format is assumed to be SDL_PIXELFORMAT_RGBA32
+	Color(uint8_t r, uint8_t g, uint8_t b); //alpha value is set to SDL_ALPHA_OPAQUE
 	Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
 
 	Color multipliedByLight(double lightMult) const; //lighting calculation must preserve alpha value
@@ -12,6 +13,6 @@ struct Color : SDL_Color
 	operator uint32_t() const;
 	Uint32 toSDL_Uint32(const uint32_t* shifts = _shift) const; //only SDL_PIXELFORMAT_RGBA32 suported!!!
 
-	static constexpr uint32_t _masks[4] = {0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000};
+	static constexpr uint32_t _masks[4] = { 0xFFu << 0, 0xFFu << 8, 0xFFu << 16, 0xFFu << 24 };
 	static constexpr uint32_t _shift[4] = { 0, 8, 16, 24 };
 };

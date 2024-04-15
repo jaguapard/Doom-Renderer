0c2192890eb375eecd595cfdaa09a71d6be5acb4
jaguapard <77740043+jaguapard@users.noreply.github.com>
2024-04-15 22:50:28 +0500

    Slight tidying triangle clipping code

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -34,51 +34,41 @@ void Triangle::drawOn(PixelBuffer<Color>& buf, const CoordinateTransformer& ctr,
 		return prepped.drawInner(buf, ctr, zBuffer, textureManager, lightMult);
 	}
 	
+	auto itBeg = std::begin(vertexOutside);
+	auto itEnd = std::end(vertexOutside);
 	if (outsideVertexCount == 1) //if 1 vertice is outside, then 1 triangle gets turned into two
 	{
-		if (Statsman::enabled) statsman.triangles.singleVertexOutOfScreenSplits++;
-		for (int i = 0; i < 3; ++i)
-		{
-			if (vertexOutside[i])
-			{
-				int v1_ind = i > 0 ? i - 1 : 2; //preserve vertice order of the original triangle and prevent out of bounds
-				int v2_ind = i < 2 ? i + 1 : 0; //we only "change" the existing vertex
+		StatCount(statsman.triangles.singleVertexOutOfScreenSplits++);
+		int i = std::find(itBeg, itEnd, true) - itBeg;
+		int v1_ind = i > 0 ? i - 1 : 2; //preserve vertice order of the original triangle and prevent out of bounds
+		int v2_ind = i < 2 ? i + 1 : 0; //we only "change" the existing vertex
 
-				const TexVertex& v1 = rot[v1_ind];
-				const TexVertex& v2 = rot[v2_ind];
-				Triangle t1 = *this, t2 = *this;
-				TexVertex clipped1 = rot[i].getClipedToPlane(v1);
-				TexVertex clipped2 = rot[i].getClipedToPlane(v2);
+		const TexVertex& v1 = rot[v1_ind];
+		const TexVertex& v2 = rot[v2_ind];
+		Triangle t1 = *this, t2 = *this;
+		TexVertex clipped1 = rot[i].getClipedToPlane(v1);
+		TexVertex clipped2 = rot[i].getClipedToPlane(v2);
 				
-				t1.tv = { v1,clipped1, v2 };
-				t2.tv = { clipped1, clipped2, v2};
+		t1.tv = { v1,clipped1, v2 };
+		t2.tv = { clipped1, clipped2, v2};
 
-				t1.drawInner(buf, ctr, zBuffer, textureManager, lightMult);
-				t2.drawInner(buf, ctr, zBuffer, textureManager, lightMult);
-				return;
-			}
-		}
+		t1.drawInner(buf, ctr, zBuffer, textureManager, lightMult);
+		t2.drawInner(buf, ctr, zBuffer, textureManager, lightMult);
+		return;
 	}
 
 	if (outsideVertexCount == 2) //in case there are 2 vertices that are outside, the triangle just gets clipped (no new triangles needed)
 	{
 		StatCount(statsman.triangles.doubleVertexOutOfScreenSplits++);
-		for (int i = 0; i < 3; ++i) //look for outside vertices and beat them into shape
-		{
-			if (!vertexOutside[i])
-			{
-				int v1_ind = i > 0 ? i - 1 : 2; //preserve vertice order of the original triangle and prevent out of bounds
-				int v2_ind = i < 2 ? i + 1 : 0; //we only "change" the existing vertex
+		int i = std::find(itBeg, itEnd, false) - itBeg;		
+		int v1_ind = i > 0 ? i - 1 : 2; //preserve vertice order of the original triangle and prevent out of bounds
+		int v2_ind = i < 2 ? i + 1 : 0; //we only "change" the existing vertex
 
-				Triangle clipped = *this;
-				clipped.tv[v1_ind] = rot[v1_ind].getClipedToPlane(rot[i]);
-				clipped.tv[v2_ind] = rot[v2_ind].getClipedToPlane(rot[i]);
-				clipped.tv[i] = rot[i];
-				return clipped.drawInner(buf, ctr, zBuffer, textureManager, lightMult);
-			}
-		}
-		
-
+		Triangle clipped = *this;
+		clipped.tv[v1_ind] = rot[v1_ind].getClipedToPlane(rot[i]);
+		clipped.tv[v2_ind] = rot[v2_ind].getClipedToPlane(rot[i]);
+		clipped.tv[i] = rot[i];
+		return clipped.drawInner(buf, ctr, zBuffer, textureManager, lightMult);
 	}
 }
 

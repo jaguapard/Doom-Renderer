0a05840ff3e98c48afeb1a59761d126745657740
jaguapard <77740043+jaguapard@users.noreply.github.com>
2024-04-16 22:18:30 +0500

    Added Ved2 and Ved3 (always double precision)

--- src/Vec.h
+++ src/Vec.h
@@ -5,5 +5,7 @@
 
 typedef bob::_Vec2<int> Vei2;
 typedef bob::_Vec2<real> Vec2;
+typedef bob::_Vec2<double> Ved2;
 typedef bob::_Vec3<int> Vei3;
 typedef bob::_Vec3<real> Vec3;
+typedef bob::_Vec3<double> Ved3;

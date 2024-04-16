3d6c25321a65aa2b76242963abb0349c0f0e106a
jaguapard <77740043+jaguapard@users.noreply.github.com>
2024-04-16 19:05:46 +0500

    Added commentary for real.h

--- src/real.h
+++ src/real.h
@@ -1,2 +1,2 @@
-#pragma once
-typedef float real;
+﻿#pragma once
+typedef float real; //allows seamless transition between double and float. Long double is unsupported, but not forbiden, it may work, IDK ¯\_(ツ)_/¯

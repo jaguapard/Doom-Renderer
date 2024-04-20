a15b0e7dc7e1aea39b179293b66ae20c7fecc6fa
jaguapard <77740043+jaguapard@users.noreply.github.com>
2024-04-20 06:34:22 +0500

    Added TextureMapper::mapTriangleRelativeToMinPoint

--- src/TextureMapper.cpp
+++ src/TextureMapper.cpp
@@ -6,6 +6,17 @@ TexVertex TextureMapper::mapRelativeToReferencePoint(const Vec3& pointToMap, con
     return mapWanted(pointToMap, delta);
 }
 
+Triangle TextureMapper::mapTriangleRelativeToMinPoint(Triangle t)
+{
+    real minX = std::min_element(t.tv.begin(), t.tv.end(), [](const TexVertex& tv1, const TexVertex& tv2) {return tv1.spaceCoords.x < tv2.spaceCoords.x; })->spaceCoords.x;
+    real minY = std::min_element(t.tv.begin(), t.tv.end(), [](const TexVertex& tv1, const TexVertex& tv2) {return tv1.spaceCoords.y < tv2.spaceCoords.y; })->spaceCoords.y;
+    real minZ = std::min_element(t.tv.begin(), t.tv.end(), [](const TexVertex& tv1, const TexVertex& tv2) {return tv1.spaceCoords.z < tv2.spaceCoords.z; })->spaceCoords.z;
+    Vec3 ref = { minX,minY, minZ };
+    for (auto& tv : t.tv)
+        tv = mapRelativeToReferencePoint(tv.spaceCoords, ref);
+    return t;
+}
+
 TexVertex TextureMapper::mapWanted(const Vec3& pointToMap, const Vec3& wanted)
 {
     constexpr real eps = 1e-6;
--- src/TextureMapper.h
+++ src/TextureMapper.h
@@ -6,5 +6,6 @@ class TextureMapper
 {
 public:
 	static TexVertex mapRelativeToReferencePoint(const Vec3& pointToMap, const Vec3& referencePoint);
+	static Triangle mapTriangleRelativeToMinPoint(Triangle t);
 	static TexVertex mapWanted(const Vec3& pointToMap, const Vec3& wanted);
 };

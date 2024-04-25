dc434179795d8343477079a411953cc606dd6fd1
jaguapard <77740043+jaguapard@users.noreply.github.com>
2024-04-26 00:01:49 +0500

    Threadpool class

---
+++ src/Threadpool.cpp
@@ -0,0 +1,68 @@
+#include "Threadpool.h"
+#include <SDL/SDL.h>
+Threadpool::Threadpool(size_t numThreads)
+{
+	for (size_t i = 0; i < numThreads; ++i)
+		threads.emplace_back(&Threadpool::workerRoutine, this, i);
+}
+size_t Threadpool::addTask(std::function<void()> taskFunc)
+{
+	std::lock_guard lck(taskListMutex);
+	this->tasks[lastTaskId++] = taskFunc;
+
+	std::unique_lock cv_lck(cv_mtx);
+	cv.notify_one();
+
+	return lastTaskId - 1;
+}
+void Threadpool::waitUntilTaskCompletes(size_t taskIndex)
+{
+	{
+		std::lock_guard tasksLck(taskListMutex);
+		if (taskIndex >= lastTaskId) throw std::runtime_error("Attempting to wait for non-existant task.");
+		auto it = finishedTasks.find(taskIndex);
+		if (it != finishedTasks.end())
+		{
+			finishedTasks.erase(it);
+			return; //if task was completed, just return
+		}
+	}
+
+	std::unique_lock cv_lck(cv_mtx);
+	cv.wait(cv_lck, [&]() {
+		std::lock_guard task_lck(taskListMutex);
+		return finishedTasks.find(taskIndex) != finishedTasks.end();
+		});
+
+	{
+		std::lock_guard tasksLck(taskListMutex);
+		finishedTasks.erase(finishedTasks.find(taskIndex));
+	}
+}
+void Threadpool::workerRoutine(size_t workerNumber)
+{
+	while (true)
+	{
+		{
+			std::unique_lock cv_lck(cv_mtx);
+			cv.wait(cv_lck, [&]() {
+				std::lock_guard<std::mutex> taskLck(taskListMutex);
+				return !tasks.empty();
+				});
+		}
+
+		std::pair<size_t, std::function<void()>> myTask;
+		{
+			std::lock_guard task_lck(taskListMutex);
+			myTask = *tasks.begin();
+			tasks.erase(tasks.begin());
+		}
+
+		myTask.second();
+		std::lock_guard task_lck(taskListMutex);
+		finishedTasks.insert(myTask.first);
+
+		std::unique_lock cv_lck(cv_mtx);
+		cv.notify_all();
+	}
+}
---
+++ src/Threadpool.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <functional>
+#include <unordered_map>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <unordered_set>
+#include <atomic>
+
+class Threadpool
+{
+public:
+	Threadpool(size_t numThreads);
+	size_t addTask(std::function<void()> taskFunc);
+	void waitUntilTaskCompletes(size_t taskIndex);
+private:
+	std::unordered_map<size_t, std::function<void()>> tasks;
+	std::unordered_set<size_t> finishedTasks;
+
+	std::mutex taskListMutex;
+	std::vector<std::thread> threads;
+	std::atomic<size_t> lastTaskId = 0;
+
+	std::condition_variable cv;
+	std::mutex cv_mtx;
+
+	void workerRoutine(size_t workerNumber);
+};

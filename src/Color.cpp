beff49e50b836ae56e984fdb86ba41a7bc497815
jaguapard <77740043+jaguapard@users.noreply.github.com>
2024-05-28 03:38:32 +0500

    Color ctors remade

--- src/Color.cpp
+++ src/Color.cpp
@@ -20,21 +20,8 @@ Color::Color(uint32_t u)
 	*this = *reinterpret_cast<const Color*>(&u);
 }
 
-Color::Color(uint8_t r, uint8_t g, uint8_t b)
-{
-	this->r = r;
-	this->g = g;
-	this->b = b;
-	this->a = SDL_ALPHA_OPAQUE;
-}
-
-Color::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
-{
-	this->r = r;
-	this->g = g;
-	this->b = b;
-	this->a = a;
-}
+Color::Color(uint8_t r, uint8_t g, uint8_t b) : SDL_Color{r,g,b,SDL_ALPHA_OPAQUE} {}
+Color::Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a) : SDL_Color{ r,g,b,a } {}
 
 Color Color::multipliedByLight(real lightMult) const
 {

04ba36bc0ce8180b75a278f97835a07a7679add8
jaguapard <77740043+jaguapard@users.noreply.github.com>
2024-05-27 13:07:46 +0500

    Ported simple color light mixing to SSE

--- src/Color.cpp
+++ src/Color.cpp
@@ -1,6 +1,7 @@
 #include <SDL/SDL.h>
 #include "Color.h"
 #include <cassert>
+//#include "Vec.h"
 
 //A bunch of sanity checks. The code in this project assumes all of this
 static_assert(sizeof(Color) == sizeof(Uint32), "Color must have the same size as SDL's Uint32");
@@ -42,47 +43,60 @@ Color Color::multipliedByLight(real lightMult) const
 
 void Color::multipliyByLightInPlace(const real* lightMults, Color* colors, int pixelCount)
 {
-#ifdef __AVX2__
 	constexpr char z = 1 << 7; //if upper bit of the 8 bit index for shuffle is set, then the element will be filled with 0's automagically.
-	
-	__m256i rShuffleMask = _mm256_broadcastsi128_si256(_mm_setr_epi8(0, z, z, z, 4, z, z, z, 8, z, z,  z, 12, z,  z,  z)); //don't care about last 16 elements, since only first 16 are used and shuffle is mirrored in both lanes
-	__m256i gShuffleMask = _mm256_add_epi8(rShuffleMask, _mm256_set1_epi32(1));
-	__m256i bShuffleMask = _mm256_add_epi8(gShuffleMask, _mm256_set1_epi32(1));
-
-	__m256i rg_mixMask = _mm256_set1_epi32(1 << 15); //blend R values into lowermost bytes of each epi32, G into 2nd lowest
-	__m256i rg_b_mixMask = _mm256_set1_epi32(1 << 23);
+#ifdef __AVX2__
+	__m256i rExtractMask = _mm256_broadcastsi128_si256(_mm_setr_epi8(0, z, z, z, 4, z, z, z, 8, z, z,  z, 12, z,  z,  z)); //don't care about last 16 elements, since only first 16 are used and shuffle is mirrored in both lanes
+	__m256i gExtractMask = _mm256_add_epi8(rExtractMask, _mm256_set1_epi32(1));
+	__m256i bExtractMask = _mm256_add_epi8(gExtractMask, _mm256_set1_epi32(1));
 
 	while (pixelCount >= 8)
 	{
 		__m256 light = _mm256_load_ps(lightMults); //load light intensities for pixels from light buffer
 		__m256i origColors = _mm256_load_si256(reinterpret_cast<const __m256i*>(colors)); //packed colors in this order: rgba,rgba,rgba,...		
 
-		//shuffle the bytes such that lowest bytes of each epi32 component in __m256i's are the values of corresponding colors
-		__m256i r = _mm256_shuffle_epi8(origColors, rShuffleMask); //lowest bytes are r values
-		__m256i g = _mm256_shuffle_epi8(origColors, gShuffleMask); //g
-		__m256i b = _mm256_shuffle_epi8(origColors, bShuffleMask); //b
+		//shuffle the bytes such that lowest bytes of each epi32 component in __m256i's are the values of corresponding colors and convert them to floats
+		__m256 floatR = _mm256_cvtepi32_ps(_mm256_shuffle_epi8(origColors, rExtractMask));
+		__m256 floatG = _mm256_cvtepi32_ps(_mm256_shuffle_epi8(origColors, gExtractMask));
+		__m256 floatB = _mm256_cvtepi32_ps(_mm256_shuffle_epi8(origColors, bExtractMask));
 		//alpha can stay broken lol
 
-		__m256 floatR = _mm256_cvtepi32_ps(r);
-		__m256 floatG = _mm256_cvtepi32_ps(g);
-		__m256 floatB = _mm256_cvtepi32_ps(b);
+		__m256i mulR = _mm256_cvtps_epi32(_mm256_mul_ps(floatR, light));
+		__m256i mulG = _mm256_cvtps_epi32(_mm256_mul_ps(floatG, light));
+		__m256i mulB = _mm256_cvtps_epi32(_mm256_mul_ps(floatB, light));
 
-		__m256 floatFinR = _mm256_mul_ps(floatR, light);
-		__m256 floatFinG = _mm256_mul_ps(floatG, light);
-		__m256 floatFinB = _mm256_mul_ps(floatB, light);
-
-		__m256i mulR = _mm256_cvtps_epi32(floatFinR);
-		__m256i mulG = _mm256_cvtps_epi32(floatFinG);
-		__m256i mulB = _mm256_cvtps_epi32(floatFinB);
-
-		//__m256i blend_rg = _mm256_blendv_epi8(mulR, mulG, rg_mixMask);
-		//__m256i res = _mm256_blendv_epi8(blend_rg, mulB, rg_b_mixMask);
 		__m256i res = _mm256_or_si256(mulR, _mm256_or_si256(_mm256_slli_epi32(mulG, 8), _mm256_slli_epi32(mulB, 16)));
-		_mm256_store_si256(reinterpret_cast<__m256i*>(colors), res);
+		*reinterpret_cast<__m256i*>(colors) = res;
 
 		lightMults += 8;
 		colors += 8;
 		pixelCount -= 8;
+	}
+#elif defined(__SSE2__)
+	__m128i rExtractMask = _mm_setr_epi8(0, z, z, z, 4, z, z, z, 8, z, z, z, 12, z, z, z);
+	__m128i gExtractMask = _mm_add_epi8(rExtractMask, _mm_set1_epi32(1));
+	__m128i bExtractMask = _mm_add_epi8(gExtractMask, _mm_set1_epi32(1));
+
+	while (pixelCount >= 4)
+	{
+		__m128 light = _mm_load_ps(lightMults); //load light intensities for pixels from light buffer
+		__m128i origColors = _mm_load_si128(reinterpret_cast<const __m128i*>(colors)); //packed colors in this order: rgba,rgba,rgba,...		
+
+		//shuffle the bytes such that lowest bytes of each epi32 component in __m256i's are the values of corresponding colors and convert them to floats
+		__m128 floatR = _mm_cvtepi32_ps(_mm_shuffle_epi8(origColors, rExtractMask));
+		__m128 floatG = _mm_cvtepi32_ps(_mm_shuffle_epi8(origColors, gExtractMask));
+		__m128 floatB = _mm_cvtepi32_ps(_mm_shuffle_epi8(origColors, bExtractMask));
+		//alpha can stay broken lol
+
+		__m128i mulR = _mm_cvtps_epi32(_mm_mul_ps(floatR, light));
+		__m128i mulG = _mm_cvtps_epi32(_mm_mul_ps(floatG, light));
+		__m128i mulB = _mm_cvtps_epi32(_mm_mul_ps(floatB, light));
+
+		__m128i res = _mm_or_si128(mulR, _mm_or_si128(_mm_slli_epi32(mulG, 8), _mm_slli_epi32(mulB, 16)));
+		*reinterpret_cast<__m128i*>(colors) = res;
+
+		lightMults += 4;
+		colors += 4;
+		pixelCount -= 4;
 	}
 #endif
 	while (pixelCount--)

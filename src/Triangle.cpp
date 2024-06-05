6c0cc7cd269ca20896f3df4df866308759b274e2
jaguapard <77740043+jaguapard@users.noreply.github.com>
2024-06-05 18:09:18 +0500

    Removed opaquePixelsMask early continue

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -209,7 +209,7 @@ void Triangle::drawSlice(const TriangleRenderContext & context, const RenderJob&
 
 			FloatPack8 opaquePixelsMask = _mm256_castsi256_ps(_mm256_cmpgt_epi32(texturePixelAlphas, _mm256_setzero_si256()));
 			opaquePixelsMask &= visiblePointsMask;
-			if (!opaquePixelsMask) continue; //if all pixels are transparent, then skip
+			//if (!opaquePixelsMask) continue; //if all pixels are transparent, then skip
 
 			_mm256_maskstore_ps(&depthBuf[pixelIndex], _mm256_castps_si256(opaquePixelsMask), zDividedUvLocal.z);
 			_mm256_maskstore_ps(&lightBuf[pixelIndex], _mm256_castps_si256(opaquePixelsMask), lightMult);

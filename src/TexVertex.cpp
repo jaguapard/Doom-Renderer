ff398fac405f88fcce1727511bc41983cc990f4c
jaguapard <77740043+jaguapard@users.noreply.github.com>
2024-06-21 12:01:04 +0500

    TexVertex operators work with G++ now

--- src/TexVertex.cpp
+++ src/TexVertex.cpp
@@ -11,17 +11,23 @@ TexVertex TexVertex::getClipedToPlane(const TexVertex& dst, real planeZ) const
 
 TexVertex TexVertex::operator-(const TexVertex& other) const
 {
-	return { .ymm = _mm256_sub_ps(ymm, other.ymm) };
+    TexVertex ret;
+    ret.ymm = _mm256_sub_ps(ymm, other.ymm);
+    return ret;
 }
 
 TexVertex TexVertex::operator+(const TexVertex& other) const
 {
-	return  { .ymm = _mm256_add_ps(ymm, other.ymm) };
+    TexVertex ret;
+	ret.ymm = _mm256_add_ps(ymm, other.ymm);
+    return ret;
 }
 
 TexVertex TexVertex::operator*(const float m) const
 {
-	return { .ymm = _mm256_mul_ps(ymm, _mm256_broadcast_ss(&m)) };
+    TexVertex ret;
+	ret.ymm = _mm256_mul_ps(ymm, _mm256_broadcast_ss(&m));
+    return ret;
 }
 
 bool TexVertex::operator<(const TexVertex& b) const
@@ -32,10 +38,12 @@ bool TexVertex::operator<(const TexVertex& b) const
 TexVertex lerp(const TexVertex& t1, const TexVertex& t2, real amount)
 {
 #ifdef __AVX__
+    TexVertex ret;
 	__m256 t = _mm256_broadcast_ss(&amount);
 	__m256 diff = _mm256_sub_ps(t2.ymm, t1.ymm); //result = tv1 + diff*amount
 	__m256 res = _mm256_fmadd_ps(diff, t, t1.ymm); //a*b+c
-	return { .ymm = res };
+	ret.ymm = res;
+    return ret;
 #endif
 	return { lerp(t1.spaceCoords, t2.spaceCoords, amount), lerp(t1.textureCoords, t2.textureCoords,amount) };
 }

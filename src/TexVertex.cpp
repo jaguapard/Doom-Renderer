50d441dea5c2552449a9bab521e14c741c4625fd
jaguapard <77740043+jaguapard@users.noreply.github.com>
2024-06-21 12:37:57 +0500

    Added ymm operators and ctors to TexVertex

--- src/TexVertex.cpp
+++ src/TexVertex.cpp
@@ -11,23 +11,17 @@ TexVertex TexVertex::getClipedToPlane(const TexVertex& dst, real planeZ) const
 
 TexVertex TexVertex::operator-(const TexVertex& other) const
 {
-    TexVertex ret;
-    ret.ymm = _mm256_sub_ps(ymm, other.ymm);
-    return ret;
+    return _mm256_sub_ps(*this, other);
 }
 
 TexVertex TexVertex::operator+(const TexVertex& other) const
 {
-    TexVertex ret;
-	ret.ymm = _mm256_add_ps(ymm, other.ymm);
-    return ret;
+    return _mm256_add_ps(*this,other);
 }
 
 TexVertex TexVertex::operator*(const float m) const
 {
-    TexVertex ret;
-	ret.ymm = _mm256_mul_ps(ymm, _mm256_broadcast_ss(&m));
-    return ret;
+    return _mm256_mul_ps(*this, _mm256_set1_ps(m));
 }
 
 bool TexVertex::operator<(const TexVertex& b) const
@@ -35,15 +29,28 @@ bool TexVertex::operator<(const TexVertex& b) const
 	return spaceCoords.y < b.spaceCoords.y;
 }
 
+TexVertex::operator __m256() const
+{
+    return std::bit_cast<__m256>(*this);
+}
+
+TexVertex::TexVertex(__m256 v)
+{
+    *this = std::bit_cast<TexVertex>(v);
+}
+
+TexVertex::TexVertex(const Vec4 space, const Vec4 texture)
+{
+    spaceCoords=space;
+    textureCoords=texture;
+}
+
 TexVertex lerp(const TexVertex& t1, const TexVertex& t2, real amount)
 {
 #ifdef __AVX__
-    TexVertex ret;
 	__m256 t = _mm256_broadcast_ss(&amount);
-	__m256 diff = _mm256_sub_ps(t2.ymm, t1.ymm); //result = tv1 + diff*amount
-	__m256 res = _mm256_fmadd_ps(diff, t, t1.ymm); //a*b+c
-	ret.ymm = res;
-    return ret;
+	__m256 diff = _mm256_sub_ps(t2, t1); //result = tv1 + diff*amount
+	return _mm256_fmadd_ps(diff, t, t1); //a*b+c
 #endif
 	return { lerp(t1.spaceCoords, t2.spaceCoords, amount), lerp(t1.textureCoords, t2.textureCoords,amount) };
 }

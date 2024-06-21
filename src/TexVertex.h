bcd0540ffc3b128fa8bea7ec0bdf22183f6de537
jaguapard <77740043+jaguapard@users.noreply.github.com>
2024-06-21 12:38:01 +0500

    Removed unions from TexVertex

--- src/TexVertex.h
+++ src/TexVertex.h
@@ -3,17 +3,14 @@
 
 struct alignas(32) TexVertex
 {
-	union {
-		struct {
-			Vec4 spaceCoords; //this can mean different things inside different contexts, world or screen space
-			Vec4 textureCoords; //this too, but it's either normal uv's, or z-divided ones
-		};
-		struct {
-			real x, y, z, w;
-			real u, v, _pad1, _pad2;
-		};
-		__m256 ymm;
-	};
+    TexVertex(const Vec4 space, const Vec4 texture);
+
+    Vec4 spaceCoords; //this can mean different things inside different contexts, world or screen space
+    Vec4 textureCoords; //this too, but it's either normal uv's, or z-divided ones
+
+    TexVertex()=default;
+    TexVertex(__m256 v);
+    operator __m256() const;
 
 	TexVertex operator-(const TexVertex& other) const;
 	TexVertex operator+(const TexVertex& other) const;

f937b86a58811db0c0fab3bf35978d39b470c560
jaguapard <77740043+jaguapard@users.noreply.github.com>
2024-06-28 19:27:08 +0500

    Fog color changed from 1,1,1,1 to 0.7,0.7,0.7,1

--- src/GameStates/MainGame.cpp
+++ src/GameStates/MainGame.cpp
@@ -209,7 +209,7 @@ void MainGame::draw()
 			}
 
 			//blitting::lightIntoFrameBuffer(*ctx.frameBuffer, *ctx.lightBuffer, myMinY, myMaxY);
-			if (settings.fogEnabled) blitting::applyFog(*ctx.frameBuffer, *ctx.zBuffer, settings.fogIntensity / settings.fovMult, Vec4(1, 1, 1, 1), myMinY, myMaxY, settings.fogEffectVersion); //divide by fovMult to prevent FOV setting from messing with fog intensity
+			if (settings.fogEnabled) blitting::applyFog(*ctx.frameBuffer, *ctx.zBuffer, settings.fogIntensity / settings.fovMult, Vec4(0.7, 0.7, 0.7, 1), myMinY, myMaxY, settings.fogEffectVersion); //divide by fovMult to prevent FOV setting from messing with fog intensity
 			threadpool->waitUntilTaskCompletes(windowUpdateTaskId);
 			blitting::frameBufferIntoSurface(*ctx.frameBuffer, wndSurf, myMinY, myMaxY, shifts);
 		};

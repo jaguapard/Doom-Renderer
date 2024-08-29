f695919d55dc2bd5dfbe92c2b6e3dcc4c1863ab9
jaguapard <77740043+jaguapard@users.noreply.github.com>
2024-08-29 11:00:11 +0500

    Removed depth-only first pass

--- src/GameStates/MainGame.cpp
+++ src/GameStates/MainGame.cpp
@@ -334,8 +334,8 @@ void MainGame::draw()
 			for (auto& it : renderJobs)
 			{
 				mfrInp.renderJobs = &it;
-				mfrInp.renderDepthTextureOnly = true;
-				mfrShaderInst.run(mfrInp);
+				//mfrInp.renderDepthTextureOnly = true;
+				//mfrShaderInst.run(mfrInp);
 
 				mfrInp.renderDepthTextureOnly = false;
 				mfrShaderInst.run(mfrInp);
